39c52e2e6e9ae56dbb3f9c3e8190b8d163ce2a9b
JJspa <JJspa@DESKTOP-053R101>
2021-10-21 10:33:59 +0200

    init comit

---
+++ readme.md
@@ -0,0 +1,5 @@
+#GitDemo
+##a git demonsration
+
+repositorys home at :
+https://github.com/JoGardhagen/GitDemo.git
---
+++ src/com/gardhagen/joakim/v42/gitDemo/Main.java
@@ -0,0 +1,10 @@
+package com.gardhagen.joakim.v42.gitDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hej hej hej ");
+
+	}
+
+}
